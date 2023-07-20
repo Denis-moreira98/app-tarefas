1c5feffa77bafe67490de68140d96e6d9efaa138
Denis Moreira <silvad281@gmail.com>
2023-07-20 15:50:49 -0300

    feat: criando login

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,10 @@
 import styles from "./styles.module.css";
 import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export function Header() {
+   const { data: session, status } = useSession();
+
    return (
       <header className={styles.header}>
          <section className={styles.content}>
@@ -11,11 +14,32 @@ export function Header() {
                      Tarefas<span>+</span>
                   </h1>
                </Link>
-               <Link href="/dashboard">
-                  <h4 className={styles.link}>Meu painel</h4>
-               </Link>
+               {session?.user && (
+                  <Link href="/dashboard">
+                     <h4 className={styles.link}>Meu painel</h4>
+                  </Link>
+               )}
             </nav>
-            <button className={styles.loginButton}>Acessar</button>
+            {status === "loading" ? (
+               <></>
+            ) : session ? (
+               <div className={styles.div_user}>
+                  <p>Olá, {session?.user?.name}</p>
+                  <button
+                     className={styles.loginButton}
+                     onClick={() => signOut()}
+                  >
+                     Sair
+                  </button>
+               </div>
+            ) : (
+               <button
+                  className={styles.loginButton}
+                  onClick={() => signIn("google")}
+               >
+                  Acessar
+               </button>
+            )}
          </section>
       </header>
    );
